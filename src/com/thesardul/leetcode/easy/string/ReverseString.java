4652b3608ecb57020fb884aa385b3eae5e8cfd42
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-07-23 00:21:49 +0530

    Reverse String

--- src/com/thesardul/leetcode/easy/string/ReverseString.java
+++ src/com/thesardul/leetcode/easy/string/ReverseString.java
@@ -1,4 +1,26 @@
 package com.thesardul.leetcode.easy.string;
 
+import java.util.Arrays;
+
 public class ReverseString {
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o','w'};
+        reverseString(s);
+
+    }
+
+    private static void reverseString(char[] s) {
+        int len = s.length;
+        int lo = 0, hi = len - 1;
+
+        while( lo <= hi){
+            char temp = s[lo];
+            s[lo] = s[hi];
+            s[hi] = temp;
+            hi--;
+            lo++;
+        }
+
+        System.out.println(Arrays.toString(s));
+    }
 }
